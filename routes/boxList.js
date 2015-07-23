393dff48fdb51a21c7b1bc6fafeb0fab3da29207
Ailrun <jjc9310@gmail.com>
2015-07-23 18:16:03 +0900

    test addBox #4

--- routes/boxList.js
+++ routes/boxList.js
@@ -40,17 +40,45 @@ const addBoxQuery = ('INSERT INTO boxList\
                      (boxName)\
                      VALUES (?);');
 function addBox(req, res, next) {
-    var usrKey = req.params.usrKey;
     var boxName = req.body.boxName;
     var queryParams = [boxName];
     connection.query(addBoxQuery, queryParams, function(error, insertInfo) {
         if (error != undefined) {
+            res.status(503).json({
+                'message' : 'there is some error in add box'
+            });
             console.log(error);
         }
         else {
+            req.body.boxKey = insertInfo.insertId;
             console.log(insertInfo);
+            addBoxOfUsr(req, res, next);
         }
     });
+}
+const addBoxOfUsrQuery = ('INSERT INTO boxOfUsrList\
+                          (boxKey, usrKey, boxIndex)\
+                          VALUES (?, ?, ?);');
+function addBoxOfUsr(req, res, next) {
+    var usrKey = req.params.usrKey;
+    var boxKey = req.body.boxKey;
+    var boxIndex = req.body.boxIndex;
+    var queryParams = [boxKey, usrKey, boxIndex];
+    connection.query(addBoxOfUsrQuery, queryParams, function(error, insertInfo) {
+        if (error != undefined) {
+            res.status(503).json({
+                'message' : 'there is some error in add box of user'
+            });
+            console.log(error);
+        }
+        else {
+            res.json({
+                'result' : true,
+                'object' : req.body
+            });
+            console.log(req.body);
+        }
+    })
 }
 router.post(addBoxURL, addBox);
 
